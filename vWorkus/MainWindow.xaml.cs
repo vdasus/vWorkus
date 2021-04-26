4d7fa772b0cf7697793b57845d4283cb7c1ee889
vdasus <me@vdasus.com>
2021-04-26 14:36:54 +0300

    way to make it WRONG (BAD style)

--- vWorkus/MainWindow.xaml.cs
+++ vWorkus/MainWindow.xaml.cs
@@ -1,17 +1,12 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Globalization;
+using System.Threading;
+using System.Timers;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using NAudio.Wave;
+using vWorkus.Properties;
+using Timer = System.Timers.Timer;
 
 namespace vWorkus
 {
@@ -20,9 +15,108 @@ namespace vWorkus
     /// </summary>
     public partial class MainWindow : Window
     {
+        private static Timer _aTimer;
+        private DateTime _endTime;
+        private int _step;
+        private string _pathToAlertFile;
+
+        private TimeSpan Remaining => _endTime - DateTime.Now;
+
+        private DateTime StartTime { get; set; }
+        
+
         public MainWindow()
         {
+            StartTime = DateTime.Now;
+
             InitializeComponent();
+
+            PrepareSettings();
+            SetTimer();
+        }
+
+        private void PrepareSettings()
+        {
+            const int MINUTE_TO_MS = 1000 * 60;
+
+            _endTime = GetTotalTimeFromSettings(Settings.Default.TotalTime);
+            _step = Settings.Default.StepInMinutes * MINUTE_TO_MS;
+            _pathToAlertFile = Settings.Default.AlertSoundPath;
+
+            Dispatcher.Invoke(() =>
+            {
+                LbCountdown.Content = GetRemainingInString();
+            });
+        }
+
+        private void SetTimer()
+        {
+            _aTimer = new Timer(_step);
+
+            _aTimer.Elapsed += OnTimedEvent;
+            _aTimer.AutoReset = true;
+            _aTimer.Enabled = true;
+        }
+
+        private void OnTimedEvent(Object source, ElapsedEventArgs e)
+        {
+            Dispatcher.Invoke(() =>
+            {
+                LbCountdown.Content = GetRemainingInString();
+            });
+
+            if (IsDone()) MakeStop();
+        }
+
+        private string GetRemainingInString()
+        {
+            return $"{Remaining.Hours:00}:{Remaining.Minutes:00}";
+        }
+
+        private DateTime GetTotalTimeFromSettings(string defaultTotalTime)
+        {
+            var tt = TimeSpan.Parse(defaultTotalTime, CultureInfo.InvariantCulture);
+            return DateTime.Now + tt;
+        }
+
+        private bool IsDone()
+        {
+            return DateTime.Now >= _endTime;
+        }
+
+        private void MakeStop()
+        {
+            _aTimer.Stop();
+
+            const string CAPTION = "vWorkus";
+
+            string messageBoxText = $"Relax now, job is done. \n{StartTime:HH.mm}-{DateTime.Now:HH:mm}";
+            MessageBoxButton button = MessageBoxButton.OK;
+            MessageBoxImage icon = MessageBoxImage.Warning;
+
+            PlayAudio(Settings.Default.AlertSoundPath);
+
+            Dispatcher.Invoke(() =>
+            {
+                LbCountdown.Foreground = Brushes.Red;
+            });
+
+            MessageBox.Show(messageBoxText, CAPTION, button, icon, MessageBoxResult.Yes);
+            
+            Environment.Exit(0);
+        }
+
+        public static void PlayAudio(string fileName)
+        {
+            using var audioFile = new AudioFileReader(fileName);
+            using var outputDevice = new WaveOutEvent();
+
+            outputDevice.Init(audioFile);
+            outputDevice.Play();
+            while (outputDevice.PlaybackState == PlaybackState.Playing)
+            {
+                Thread.Sleep(1000);
+            }
         }
     }
 }
